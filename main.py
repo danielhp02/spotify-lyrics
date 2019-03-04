377cf84611b7034a7f256f249f90acaa9bf5b6ae
Daniel Hewitt-Perraud <danielhp@optusnet.com.au>
2019-03-04 22:48:50 +1100

    Cleaned up the Spotify track input and console output into functions

--- main.py
+++ main.py
@@ -11,11 +11,37 @@ else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 
+# Define functions
 def get_artist(song):
     artists = []
     for artist in song['artists']:
         artists.append(artist['name'])
     return artists
+
+def get_track(sp, text_output=False): # if text_output is False, it will output a dict
+    current_playback = sp.current_playback()['item']
+
+    if current_playback is not None:
+        return {'name':  current_playback['name'],
+                'artist': get_artist(current_playback)}
+    else:
+        return {'name':  None,
+                'artist': None}
+
+
+def print_track(current_song): # Change later, maybe to output a string or list of formatted strings (eg. [title, [artist1, artist2]])
+    if len(current_song['artist']) == 1:
+        print(current_song['name'], 'by', str(current_song['artist'][0]), 'is now playing.')
+    else:
+        artist_string = [current_song['artist'][0]]
+        for index in range(1, len(current_song['artist'])):
+            artist = current_song['artist'][index]
+            if artist == current_song['artist'][-1]:
+                artist_string.append(' and ' + str(artist))
+            else:
+                artist_string.append(', ' + str(artist))
+        artist_string = ''.join(artist_string)
+        print(current_song['name'], 'by', str(artist_string), 'is now playing.')
 
 with open('./tokens.json', 'r') as json_file:
     tokens = json.load(json_file)
@@ -30,24 +56,12 @@ if token:
     running = True
     try:
         while running:
-            current_playback = sp.current_playback()['item']
+            last_song = current_song
+            current_song = get_track(sp)
 
-            if current_playback is not None:
-                if current_playback['name'] != current_song['name']:
-                    current_song = {'name':  current_playback['name'],
-                                    'artist': get_artist(current_playback)}
-                    if len(current_song['artist']) == 1:
-                        print(current_song['name'], 'by', str(current_song['artist'][0]), 'is now playing.')
-                    else:
-                        artist_string = [current_song['artist'][0]]
-                        for index in range(1, len(current_song['artist'])):
-                            artist = current_song['artist'][index]
-                            if artist == current_song['artist'][-1]:
-                                artist_string.append(' and ' + str(artist))
-                            else:
-                                artist_string.append(', ' + str(artist))
-                        artist_string = ''.join(artist_string)
-                        print(current_song['name'], 'by', str(artist_string), 'is now playing.')
+            if last_song['name'] != current_song['name']:
+                print_track(current_song)
+
     except KeyboardInterrupt:
         print("\nClosing...")
         sys.exit()
