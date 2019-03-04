7c6947ecdccb0428cc29e20d88fb1c67fa2ab788
Daniel Hewitt-Perraud <danielhp@optusnet.com.au>
2019-03-04 23:34:15 +1100

    Can now get lyrics from Genius and print out the lyrics.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import json
 import spotipy
 import spotipy.util as util
+import lyricsgenius
 
 scope = 'user-read-playback-state'
 
@@ -28,7 +29,6 @@ def get_track(sp, text_output=False): # if text_output is False, it will output 
         return {'name':  None,
                 'artist': None}
 
-
 def print_track(current_song): # Change later, maybe to output a string or list of formatted strings (eg. [title, [artist1, artist2]])
     if len(current_song['artist']) == 1:
         print(current_song['name'], 'by', str(current_song['artist'][0]), 'is now playing.')
@@ -43,10 +43,13 @@ def print_track(current_song): # Change later, maybe to output a string or list 
         artist_string = ''.join(artist_string)
         print(current_song['name'], 'by', str(artist_string), 'is now playing.')
 
+# Load tokens
 with open('./tokens.json', 'r') as json_file:
     tokens = json.load(json_file)
 
-token = util.prompt_for_user_token(username, scope, tokens['client_id'], tokens['client_secret'], tokens['redirect_uri'])
+# Authenticate Spotify and Genius
+token = util.prompt_for_user_token(username, scope, tokens["spotify"]['client_id'], tokens["spotify"]['client_secret'], tokens["spotify"]['redirect_uri'])
+genius = lyricsgenius.Genius(tokens["genius"]["token"])
 
 if token:
     sp = spotipy.Spotify(auth=token)
@@ -61,6 +64,8 @@ if token:
 
             if last_song['name'] != current_song['name']:
                 print_track(current_song)
+                genius_song = genius.search_song(current_song["name"], current_song["artist"][0])
+                print(genius_song.lyrics)
 
     except KeyboardInterrupt:
         print("\nClosing...")
