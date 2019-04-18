686b7f1ae2dcbe09f6b5d769e413d31100eb1d63
Daniel Hewitt-Perraud <danielhp@optusnet.com.au>
2019-04-18 22:59:45 +1000

    Working on problems relating to no songs playing

--- spogen/__init__.py
+++ spogen/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
 
     # load the instance config, if it exists, when not testing
     app.config.from_pyfile('config.py', silent=True) if test_config is None else app.config.from_mapping(test_config)
-    
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -24,14 +24,14 @@ def create_app(test_config=None):
 
     # ensure tokens.json exists
     # doesn't actually halt execution, just catches ugly error and packages it nicely
-    if not os.path.isfile('./tokens.json'): sys.exit('ERROR: tokens.json not found!') 
+    if not os.path.isfile('./tokens.json'): sys.exit('ERROR: tokens.json not found!')
 
     from . import db
     db.init_app(app)
 
     from . import lyrics
     objects = lyrics.init(spotipy, util, lyricsgenius)
-    
+
     # @app.route('/')
     # def root():
     #     if lyrics.get_playing_status(objects['spotipy']):
@@ -44,16 +44,21 @@ def create_app(test_config=None):
     @app.route('/_get_music_data')
     def get_music_data():
         song_details = lyrics.get_song_data(objects["spotipy"], objects["genius"])
-        if song_details != None:
+        if song_details != None and song_details != "nothing playing":
+            print("there is a song")
             return jsonify(songname = song_details['songname'], lyrics = song_details['lyrics'])
+        elif song_details == "nothing playing":
+            return jsonify(songname = '', lyrics = '')
         else:
             return jsonify(songname = '', lyrics = 'Adding song to database...')
 
     @app.route('/')
     def root():
         if lyrics.get_playing_status(objects['spotipy']):
+            print("something playing")
             return render_template('base.html')
         else:
+            print("nothing playing")
             return render_template('noneplaying.html')
-    
+
     return app
--- spogen/lyrics.py
+++ spogen/lyrics.py
@@ -55,42 +55,46 @@ def get_playing_status(sp):
     current_playback = sp.current_playback()
     if current_playback != None:
         return True if current_playback['currently_playing_type'] == 'track' else False
-    else: 
+    else:
         return False
 
 def get_song_data(sp, genius):
     global adding_to_db
 
-    current_playback = sp.current_playback()['item']
-    song_data = {
-        'songname': current_playback['name'],
-        'songid': current_playback['id'],
-        'album': current_playback['album']['name'],
-        'albumid': current_playback['album']['id'],
-        'albumartistid': current_playback['artists'][0]['id'],
-        'artist': get_artists(current_playback['artists'], 'name'),
-        'artistid': get_artists(current_playback['artists'], 'id'),
-        'albumart': get_art(current_playback),
-        'videolink': '0'
-    }
-    if db.query_db('SELECT SONGID FROM SONG WHERE SONGID = ?', (song_data['songid'],)) == []:
-        if adding_to_db != True:
-            print('Song: %s not found in database. Adding...' % song_data['songname'])
-            adding_to_db = True
-            db.query_db('INSERT INTO SONG (SONGID, SONGNAME, LYRICS) VALUES (?, ?, ?)', (song_data['songid'], song_data['songname'], get_lyrics(genius, song_data)))
+    try:
+        current_playback = sp.current_playback()['item']
+        song_data = {
+            'songname': current_playback['name'],
+            'songid': current_playback['id'],
+            'album': current_playback['album']['name'],
+            'albumid': current_playback['album']['id'],
+            'albumartistid': current_playback['artists'][0]['id'],
+            'artist': get_artists(current_playback['artists'], 'name'),
+            'artistid': get_artists(current_playback['artists'], 'id'),
+            'albumart': get_art(current_playback),
+            'videolink': '0'
+        }
+        if db.query_db('SELECT SONGID FROM SONG WHERE SONGID = ?', (song_data['songid'],)) == []:
+            if adding_to_db != True:
+                print('Song: %s not found in database. Adding...' % song_data['songname'])
+                adding_to_db = True
+                db.query_db('INSERT INTO SONG (SONGID, SONGNAME, LYRICS) VALUES (?, ?, ?)', (song_data['songid'], song_data['songname'], get_lyrics(genius, song_data)))
+            else:
+                return None
         else:
-            return None
-    else:
-        print('Song: %s found in database.' % song_data['songname'])
-    
-    db.get_db().commit()
-    song_data['lyrics'] = db.query_db('SELECT LYRICS FROM SONG WHERE SONGID = ?', (song_data['songid'],))[0]['lyrics'].replace('\n', '<br>') 
-    
-    out = ''
-    for s, t in zip(song_data['artistid'], song_data['artist']):
-        out += ('<a href=\'https://open.spotify.com/artist/{0}\'>{1}</a>'.format(s, t))
-        out += ', '
-    song_data['artistlinks'] = out[0:-2]
+            print('Song: %s found in database.' % song_data['songname'])
 
-    adding_to_db = False
-    return song_data
+        db.get_db().commit()
+        song_data['lyrics'] = db.query_db('SELECT LYRICS FROM SONG WHERE SONGID = ?', (song_data['songid'],))[0]['lyrics'].replace('\n', '<br>')
+
+        out = ''
+        for s, t in zip(song_data['artistid'], song_data['artist']):
+            out += ('<a href=\'https://open.spotify.com/artist/{0}\'>{1}</a>'.format(s, t))
+            out += ', '
+        song_data['artistlinks'] = out[0:-2]
+
+        adding_to_db = False
+        return song_data
+    except TypeError:
+        print("No song is currently playing.")
+        return "nothing playing" # eh should work
